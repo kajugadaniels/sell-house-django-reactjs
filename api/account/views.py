91edea38b9a01ec672fc69bb0b40c2cd3b9a5f2a
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-19 20:57:05 +0200

    Add register a new user functionality

--- api/account/views.py
+++ api/account/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class LoginView(GenericAPIView):  # Change to GenericAPIView
     permission_classes = [permissions.AllowAny]
@@ -31,6 +31,30 @@ class LoginView(GenericAPIView):  # Change to GenericAPIView
                 'message': 'Login successful.'
             }, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid email or password.'}, status=status.HTTP_400_BAD_REQUEST)
+
+class RegisterView(generics.CreateAPIView):
+    """
+    View to register a new user. Accessible without authentication.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # Allows anyone to access this view without authentication
+
+    def create(self, request, *args, **kwargs):
+        # Serialize the request data
+        serializer = self.get_serializer(data=request.data)
+        # Validate and save the user
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "message": "User registered successfully."
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response({
+                "message": "User registration failed.",
+                "errors": serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
