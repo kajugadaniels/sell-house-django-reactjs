c3873546edb7f25319c6bb35c2ad1e44a3a22590
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-19 21:49:42 +0200

    New changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminRoutes from './admin/Routes.jsx';
 import UserRoutes from './user/Routes.jsx';
-import AdminLayout from './layouts/AdminLayout.jsx'
-import UserLayout from './layouts/UserLayout.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
     return (
@@ -17,6 +17,17 @@ const App = () => {
                 } />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={true}
+                newestOnTop
+                closeOnClick
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </Router>
     )
 }
