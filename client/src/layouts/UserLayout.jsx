b004463f9d75507b8aa5bfbc5a1c12aa0a9304f3
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-19 22:05:42 +0200

    New changes

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const UserLayout = () => {
     return (
-        <div>UserLayout</div>
+        <div>
+            UserLayout
+            <Outlet />
+        </div>
     )
 }
 
