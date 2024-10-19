6c254b0e6b0e3dca0b0b97e37a3241ce406fd017
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-19 21:47:23 +0200

    Separate routes from admin and user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminRoutes from './admin/Routes.jsx';
 import UserRoutes from './user/Routes.jsx';
+import AdminLayout from './layouts/AdminLayout.jsx'
+import UserLayout from './layouts/UserLayout.jsx'
 
 const App = () => {
     return (
-        <div>App</div>
+        <Router>
+            <Routes>
+                <Route path='/admin' element={
+                    <AdminLayout>
+                        <AdminRoutes />
+                    </AdminLayout>
+                } />
+                <Route path='/*' element={
+                    <UserLayout>
+                        <UserRoutes />
+                    </UserLayout>
+                } />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </Router>
     )
 }
 
