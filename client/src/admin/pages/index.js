12141ac99e8f1bfd246a580a55991265a09c51db
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-20 21:43:36 +0200

    Setup GetProjects page

--- client/src/admin/pages/index.js
+++ client/src/admin/pages/index.js
@@ -3,3 +3,5 @@ export { default as Login } from './auth/Login'
 export { default as Dashboard } from './Dashboard'
 export { default as Profile } from './Profile'
 export { default as Contacts } from './Contacts'
+
+export { default as GetProjects } from './projects/GetProjects'
