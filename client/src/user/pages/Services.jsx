920e2c0c5cd0cb35458737d4cc63741a8dc9a6dd
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-20 17:27:34 +0200

    Add jumbotron on services page

--- client/src/user/pages/Services.jsx
+++ client/src/user/pages/Services.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Services = () => {
     return (
-        <div>Services</div>
+        <>
+            <div className="breadcrumb-area pt-50">
+                <div className="container">
+                    <div className="row">
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                                <li className="breadcrumb-item active" aria-current="page">Services</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
