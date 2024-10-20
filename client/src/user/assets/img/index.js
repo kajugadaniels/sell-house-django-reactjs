f5da7c41a784402acaef5e811c3655d23a018acf
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-20 15:10:18 +0200

    Add services section

---
+++ client/src/user/assets/img/index.js
@@ -0,0 +1,3 @@
+export { default as Design } from './interior-design_resize.png'
+export { default as Develop } from './think_resize.png'
+export { default as Construct } from './engineer_resize.png'
--- client/src/user/pages/Home.jsx
+++ client/src/user/pages/Home.jsx
@@ -1,12 +1,60 @@
 import React from 'react'
 import { Hero } from '../components'
+import { Construct, Design, Develop } from '../assets/img'
 
 const Home = () => {
     return (
-        <div>
+        <>
             <Hero />
-            Home
-        </div>
+
+            <div id="feature-2" className="feature-area gray-bg section-padding pb-100">
+                <div className="container">
+                    <div className='row'>
+                        <div className="section-title text-low">
+                            <div>
+                                <h2>What We Do</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
+                            <div className="single-feature-item">
+                                <div className="feature-icon">
+                                    <img src={Design} alt="" />
+                                </div>
+                                <div className="feature-content">
+                                    <h5>Design</h5>
+                                    <p>Welink is committed to refined and innovative design that stands the test of time.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
+                            <div className="single-feature-item">
+                                <div className="feature-icon">
+                                    <img src={Develop} alt="" />
+                                </div>
+                                <div className="feature-content">
+                                    <h5>Develop</h5>
+                                    <p>Welink is committed to creating living spaces that are smart, unique, memorable and sustainable.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
+                            <div className="single-feature-item">
+                                <div className="feature-icon">
+                                    <img src={Construct} alt="" />
+                                </div>
+                                <div className="feature-content">
+                                    <h5>Construct</h5>
+                                    <p>Welink is committed to delivering exceptional homes on time and on budget every single time.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
