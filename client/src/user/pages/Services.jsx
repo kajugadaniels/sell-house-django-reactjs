8e26b0e346cd40fda06a59beb3fbce4adafbe9c6
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-20 17:48:17 +0200

    Design services page

--- client/src/user/pages/Services.jsx
+++ client/src/user/pages/Services.jsx
@@ -16,6 +16,183 @@ const Services = () => {
                     </div>
                 </div>
             </div>
+
+            <div id="price_page" className="hero-area">
+                <div className="container">
+                    <div className="row hero-area-inner align-items-end">
+                        <div className="col-xl-9 col-lg-9 col-md-10">
+                            <div className="hero-area-content">
+                                <div className="section-title">
+                                    <h1>Develop <br /></h1>
+                                    <h1>With Us!! <br /></h1>
+                                </div>
+                                <p>
+                                    At WeLink Home, we hold your hands every step of the way. Say bye to sleepless nights.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="process-3" className="pt-20 pb-0 process-section section-padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12">
+                            <div className="mt-40 row">
+                                <div className="col-xl-5 col-lg-5 col-md-5 wow fadeInUp animated" data-wow-delay=".2s">
+                                    <div className="section-title">
+                                        <h6>Enjoy the Journey</h6>
+                                        <h2>Your Development Process</h2>
+                                        <p>
+                                            First home buyer? Knock down rebuild? Elite property investor? We can help. We want to be as transparent with you as possible so you enjoy the process more because you know what to expect next.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".2s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>01</span>
+                                        </div>
+                                        <h4>
+                                            Let's start <br /> with hello_
+                                        </h4>
+                                        <p>
+                                            <b>Meet and greet:</b> Set up a meeting with one of our specialists and chat through options of your development.
+                                        </p>
+                                        <p>
+                                            <b>Proposals:</b> We present you all the options to you within 7 days after research is professionally conducted.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-3 col-lg-3 col-md-3 wow fadeInUp animated" data-wow-delay=".4s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>02</span>
+                                        </div>
+                                        <h4>
+                                            Paint <br /> your vision_
+                                        </h4>
+                                        <p>
+                                            <b>Decision making:</b> We walk you through all the options and scenarios for the development.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".6s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>03</span>
+                                        </div>
+                                        <h4>
+                                            The numbers <br /> game_
+                                        </h4>
+                                        <p>
+                                            <b>Calculate:</b> We present you a full feasibility report with relevant data freshly collected.
+                                        </p>
+                                        <p>
+                                            <b>Drawing board:</b> Our design team will prepare a selection of scenarios for the development.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>04</span>
+                                        </div>
+                                        <h4>
+                                            Commit to <br /> the project_
+                                        </h4>
+                                        <p>
+                                            <b>Pull the trigger:</b> You decide, we execute. By this stage, we have cleared our mind and both you and Welink are very committed to making it a success.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>05</span>
+                                        </div>
+                                        <h4>
+                                            Design <br /> approval_
+                                        </h4>
+                                        <p>
+                                            <b>DA granted:</b> After weeks, sometimes months of hard work, we are now being supported by your local authorities for the development.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>06</span>
+                                        </div>
+                                        <h4>
+                                            Construction <br /> permit_
+                                        </h4>
+                                        <p>
+                                            <b>Building permit granted:</b> Welink team is focused during the process of documentation. 100% dedicated team ensures 100% success on site.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>07</span>
+                                        </div>
+                                        <h4>
+                                            Construction <br /> underway_
+                                        </h4>
+                                        <p>
+                                            <b>Exciting times ahead:</b> Congralutaions! Say bye to paperwork (at least for you). Now you will see how fast and accurate we deliver a project.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>08</span>
+                                        </div>
+                                        <h4>
+                                            Hand <br /> over_
+                                        </h4>
+                                        <p>
+                                            <b>Completion Inspection:</b> You have been waiting for this big day. Your dedicated construction manager will present your home or homes to you.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>09</span>
+                                        </div>
+                                        <h4>
+                                            Get it sold <br /> or move in_
+                                        </h4>
+                                        <p>
+                                            <b>The Start:</b> If you are moving in, this is the start of your new chapter. We will always be here to answer your questions.
+                                        </p>
+                                        <p>
+                                            <b>The End:</b> If we are selling these properties for you, this is the end of your project. We will get them sold while you are getting ready for your next project with Welink.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated" data-wow-delay=".8s">
+                                    <div className="single-process-wrap">
+                                        <div className="process-num">
+                                            <span>10</span>
+                                        </div>
+                                        <h4>
+                                            Aftercare_
+                                        </h4>
+                                        <p>
+                                            <b>Genuine customer service:</b> Once you have lived in your brand new home, we are obliged to perform a 6 months defects maintenance for you. You are welcome to contact us at any time should you wish to get them attended sooner.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
