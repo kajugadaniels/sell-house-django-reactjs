a9aad05dd916a2ee0628b0a6641d139c7149dfab
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-20 21:08:00 +0200

    Add projects model

--- api/home/models.py
+++ api/home/models.py
@@ -1,4 +1,9 @@
+import os
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class Contact(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -8,3 +13,39 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+def project_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'projects/project_{slugify(instance.title)}_{instance.category}_{instance.created_at}{file_extension}'
+
+class Project(models.Model):
+    CATEGORY_CHOICES = [
+        ('Residential', 'Residential'),
+        ('Commercial', 'Regular Projects'),
+        ('Selling', 'Selling'),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    image = ProcessedImageField(
+        upload_to=project_image_path,
+        processors=[ResizeToFill(1270, 1270)],
+        format='JPEG',
+        options={'quality': 90},
+    )
+    total_area = models.CharField(max_length=255, null=True, blank=True)
+    living_space = models.CharField(max_length=255, null=True, blank=True)
+    price = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    year = models.CharField(max_length=255, null=True, blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Project, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
