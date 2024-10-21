92903b5d86d861c21a9333232f32fe9ac207a335
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-21 17:14:07 +0200

    New changes

--- client/src/user/pages/Home.jsx
+++ client/src/user/pages/Home.jsx
@@ -9,46 +9,62 @@ const Home = () => {
         <>
             <Hero />
 
-            <div id="feature-2" className="feature-area gray-bg section-padding pb-100">Z
+            <div className="pt-12 pb-12 gray-bg feature-section section-padding">
                 <div className="container">
-                    <div className='row'>
-                        <div className="section-title text-low">
-                            <div>
-                                <h2>What We Do</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
-                            <div className="single-feature-item">
-                                <div className="feature-icon">
-                                    <img src={Design} alt="" />
-                                </div>
-                                <div className="feature-content">
-                                    <h5>Design</h5>
-                                    <p>Welink is committed to refined and innovative design that stands the test of time.</p>
+                    <div className="row gx-5">
+                        <div className="col-xl-5 col-lg-4">
+                            <div className="section-title text-low">
+                                <div>
+                                    <h2 className='text-black fw-bold'>What We Do</h2>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
-                            <div className="single-feature-item">
-                                <div className="feature-icon">
-                                    <img src={Develop} alt="" />
+                        <div className="col-xl-7 col-lg-8">
+                            <div className="row gy-5 feature-wrap">
+                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 wow fadeInUp animated" data-wow-delay=".2s">
+                                    <div className="single-feature-item">
+                                        <div className="single-feat-inner">
+                                            <div className="icon-wrap">
+                                                <img src={Design} alt="Design" />
+                                            </div>
+                                            <div className="service-title">
+                                                <h4>Design</h4>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            Welink is committed to refined and innovative design that stands the test of time.
+                                        </p>
+                                    </div>
                                 </div>
-                                <div className="feature-content">
-                                    <h5>Develop</h5>
-                                    <p>Welink is committed to creating living spaces that are smart, unique, memorable and sustainable.</p>
+                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 wow fadeInUp animated" data-wow-delay=".4s">
+                                    <div className="single-feature-item">
+                                        <div className="single-feat-inner">
+                                            <div className="icon-wrap">
+                                                <img src={Develop} alt="Develop" />
+                                            </div>
+                                            <div className="service-title">
+                                                <h4>Develop</h4>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            Welink is committed to creating living spaces that are smart, unique, memorable and sustainable.
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-lg-4 col-md-4 wow fadeInUp animated">
-                            <div className="single-feature-item">
-                                <div className="feature-icon">
-                                    <img src={Construct} alt="" />
-                                </div>
-                                <div className="feature-content">
-                                    <h5>Construct</h5>
-                                    <p>Welink is committed to delivering exceptional homes on time and on budget every single time.</p>
+                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 wow fadeInUp animated" data-wow-delay=".6s">
+                                    <div className="single-feature-item">
+                                        <div className="single-feat-inner">
+                                            <div className="icon-wrap">
+                                                <img src={Construct} alt="Construct" />
+                                            </div>
+                                            <div className="service-title">
+                                                <h4>Construct</h4>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            Welink is committed to delivering exceptional homes on time and on budget every single time.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -72,7 +88,7 @@ const Home = () => {
                         <div className="col-xl-6 col-lg-6">
                             <div className="about-content-wrapper wow fadeInUp animated">
                                 <div className="section-title text-low">
-                                    <h2 className='text-black'>About WeLink Home</h2>
+                                    <h2 className='text-black fw-bold'>About WeLink Home</h2>
                                 </div>
                                 <div className="p-animation">
                                     <p>
@@ -86,34 +102,100 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="project-area gray-bg section-padding">
+            <div className="feature_slider_wrap gray-bg border-top border-bottom pt-60 pb-60">
+                <div className="feature_item">
+                    <h2>
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        Design for Sustainability
+                    </h2>
+                    <h2 className="stroke">
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        Innovative Design
+                    </h2>
+                    <h2>
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        Experienced Team
+                    </h2>
+                    <h2 className="stroke">
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        Honesty & Integrity
+                    </h2>
+                    <h2>
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        24/7 Accessibility
+                    </h2>
+                    <h2 className="stroke">
+                        <img src="https://capricorn-theme.com/html/architon/assets/img/asterisk-dark.png" alt="feat-icon" />
+                        Quality Craftsmanship
+                    </h2>
+                </div>
+            </div>
+
+            <div className="team-section section-padding pb-90">
                 <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="text-center col-xl-7 col-lg-7 col-md-9">
-                            <div className="section-title">
-                                <h2>Discover our latest projects</h2>
+                    <div className="row border-bottom">
+                        <div className="col-xl-7 col-lg-7">
+                            <div className="section-title text-low">
+                                <h2 className='text-black fw-bold'>
+                                    Discover our <br /> latest projects
+                                </h2>
                             </div>
-                            <p>
-                                Combine our knowledge, experience and award-winning designs with the dedicated in-house construction team, we put smiles on the faces of all our satisfied customers.
-                            </p>
-                            <Link to='/projects'>
-                                <h6>See More</h6>
-                            </Link>
+                        </div>
+                        <div className="col-xl-5 col-lg-5">
+                            <div className="right-content text-lg-end">
+                                <p>
+                                    Combine our knowledge, experience and award-winning designs with the dedicated in-house construction team, we put smiles on the faces of all our satisfied customers.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div className="container-fluid">
-                    <div className="grid grid-cols-4 gap-4 mt-10 project-grid">
-                        {/* Project Item 1 */}
-                        <div className="bg-cover single-project-item" style={{ backgroundImage: "url('https://capricorn-theme.com/html/architon/assets/img/project/project-details/project-details.jpg')" }}>
-                            <div className="project-inner">
-                                <a href="#" className="project-icon">
-                                    <Plus className="las la-plus" />
-                                </a>
-                                <div className="hover-info">
-                                    <h4>Name Residence</h4>
+                    <div className="mt-40 row gx-5">
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                            <div className="single-team-wrap">
+                                <img src="https://capricorn-theme.com/html/architon/assets/img/project/project-details/project-details.jpg" alt="" />
+                                <div className="team-info">
+                                    <span>
+                                        Location
+                                    </span>
+                                    <span> 
+                                        Name Residence
+                                    </span>
                                 </div>
+                                <p className="designation">
+                                    Price: 400 RWF
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                            <div className="single-team-wrap">
+                                <img src="https://capricorn-theme.com/html/architon/assets/img/project/project-details/project-details.jpg" alt="" />
+                                <div className="team-info">
+                                    <span>
+                                        Location
+                                    </span>
+                                    <span> 
+                                        Name Residence
+                                    </span>
+                                </div>
+                                <p className="designation">
+                                    Price: 400 RWF
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                            <div className="single-team-wrap">
+                                <img src="https://capricorn-theme.com/html/architon/assets/img/project/project-details/project-details.jpg" alt="" />
+                                <div className="team-info">
+                                    <span>
+                                        Location
+                                    </span>
+                                    <span> 
+                                        Name Residence
+                                    </span>
+                                </div>
+                                <p className="designation">
+                                    Price: 400 RWF
+                                </p>
                             </div>
                         </div>
                     </div>
