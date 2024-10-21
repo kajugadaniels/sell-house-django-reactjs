944605568576a1437a3090d65c72f67c1512d8ca
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-21 11:03:55 +0200

    Add image functionality in ProjectSerialzer

--- api/home/serializers.py
+++ api/home/serializers.py
@@ -9,6 +9,20 @@ class ContactSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'email', 'phone_number', 'message']
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+
     class Meta:
         model = Project
-        fields = ['id', 'title', 'slug', 'total_area', 'living_space', 'price', 'location', 'year', 'category', 'description']
+        fields = ['id', 'title', 'slug', 'total_area', 'living_space', 'price', 'location', 'year', 'category', 'description', 'image']
+
+    def update(self, instance, validated_data):
+        # Handle image upload separately if provided
+        if 'image' in validated_data:
+            instance.image = validated_data.pop('image')
+
+        # Update the other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        instance.save()
+        return instance
